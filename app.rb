674bc5bc0a61588c6525927218c439a4324100a6
EC2 Default User <ec2-user@ip-172-31-22-217.us-east-2.compute.internal>
2018-07-22 09:13:01 +0000

    First Commit

---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+
+get '/' do
+    @now = Time.now
+    erb :index
+end
+
+get '/contact_new' do
+    erb :contact_form
+end
+
+post '/contacts' do
+    puts "送信されたデータ"
+    p params
+    redirect '/'
+end
