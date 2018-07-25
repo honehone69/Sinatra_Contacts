d70ecb5d8f530e46615b7adf2ae18b2b9ac51bce
EC2 Default User <ec2-user@ip-172-31-22-217.us-east-2.compute.internal>
2018-07-25 10:53:36 +0000

    add Database

--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
-require 'sinatra'
-require 'sinatra/reloader'
+require 'rubygems'
+require 'bundler'
 
+Bundler.require
+
+set :database, {adapter: "sqlite3", database: "contacts.foo.sqlite3"}
+
+class Contact < ActiveRecord::Base
+  validates_presence_of :name
+end
 
 get '/' do
     @now = Time.now
+    @contacts = Contact.all
     erb :index
 end
 
@@ -14,5 +22,12 @@ end
 post '/contacts' do
     puts "送信されたデータ"
     p params
+    
+    name = params[:name]
+    
+    #DBに保存
+    contact = Contact.new({name: name})
+    contact.save
+    
     redirect '/'
 end
